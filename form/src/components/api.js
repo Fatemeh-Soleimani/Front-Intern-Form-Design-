71e7cc4cc3d0bf4f404be597292b13e11ed0c6e7
Fatemeh-Soleimani <fsoleimani79@yahoo.com>
2022-08-18 23:39:52 +0430

    add get and update method(post,not complete) with bug in assign object

---
+++ form/src/components/api.js
@@ -0,0 +1,33 @@
+import { resolveOnChange } from "antd/lib/input/Input";
+import axios from "axios";
+export async function getrequest() {
+	return new Promise((resolve) => {
+		axios.get('https://ansarcrm.iran.liara.run/MainApi/getForm?id=1000', {
+			params: {
+				ID: 1000
+			}
+		})
+			.then(function (response) {
+				resolve(response.data);
+				//console.log(response.data);
+			})
+	})
+}
+
+export async function update(list) {
+	return new Promise((resolve) => {
+		axios.patch('https://ansarcrm.iran.liara.run/MainApi/setForm', list)
+			.then(function (response) {
+
+				console.log(response.data);
+			});
+	})
+}
+export async function postrequest(e){
+	//not complete............
+
+	
+	// e.preventDesfault();
+		// const userdata={Name:name , description:desc,prior:prio};
+		// await axios.post('https://ansarcrm.iran.liara.run/MainApi/setForm',JSON.stringify(userdata)).then(result=>{console.log(result.data);});
+}
--- form/src/components/myform.js
+++ form/src/components/myform.js
@@ -1,129 +1,162 @@
+import React, { setState } from 'react';
 import "./myform.css";
 import { Form, Input, Button, Row, Col, Switch } from "antd";
 import TextArea from "antd/lib/input/TextArea";
 import FormItem from "antd/lib/form/FormItem";
-import { useState } from "react";
+import { useState} from "react";
 import axios from "axios";
 import { OmitProps } from "antd/lib/transfer/ListBody";
 // const [users, setUsers] = useState([]);
-async function getrequest(){
-	axios.get('https://ansarcrm.iran.liara.run/MainApi/getForm?id=1000', {
-		params: {
-		  ID: 1000
-		}
-	  })
-	  .then(function (response) {
-		
-		console.log(response.data);
-	  });
-	}
-	
+import { getrequest, postrequest, update } from "./api.js";
+
 function MyForm() {
+	 let values=[];
+	//let values = [{"_id":"1000","name":"test","desc":"test description","priority":100,"author":"test","created":"","lastModified":"","state":true}];
+	getrequest().then((listOfValue) => {
+		console.log(listOfValue);
+		values=JSON.parse(JSON.stringify(listOfValue));
+		//this.setState({ values: listOfValue.result })
+		//values={...listOfValue.result};
+		
+	});
+	//console.log(this.values);
 
-	getrequest();
-	const [name,setname]=useState('');
-	const [desc,setdesc]=useState('');
-	const [prio,setprio]=useState('');
-	const [author,setauthor]=useState('');
-	const [create,setcreate]=useState('');
-	const [lastm,setlastm]=useState('');
-	
+	console.log(values);
+	const handleFormSubmit = (input) => {
+		//let Date=new Date().toLocaleString();
+		if (input.state === undefined) {
+			input.state = true;
+		}
+		if (input.description === undefined) {
+			input.description = "";
+		}
+		//input.lastmodified=Date;
+		input.created = values[0].created;
+		input.author = values[0].author;
 
-const handlename=(event)=>{
-	// this.setState({name: event.target.value })
-	const Name=event.target.value;
-	console.log(Name);
-	setname(Name);
-}
-const handledesc=(event)=>{
-	const description=event.target.value;
-	console.log(description);
-	setname(description);
-}
-const handleprio=(event)=>{
-	const prior=event.target.value;
-	console.log(prior);
-	setname(prior);
-}
-const submituser=async(e)=>{
-	
-	e.preventDesfault();
-	const userdata={Name:name , description:desc,prior:prio};
-	await axios.post('https://ansarcrm.iran.liara.run/MainApi/setForm',JSON.stringify(userdata)).then(result=>{console.log(result.data);});
-	
-}
-	return <div><Form onSubmit={submituser} name='basic form' layout="vertical" size="middle">
-		<h1 class="title">General Setting</h1>
-		<Row gutter={[64, 40]}>
-			<Col span={12} ><div>
-				<Form.Item label="Name" name="Name" rules={[{ required: true, message: 'Please enter your name' }]}>
-					<input name="name" className="name" onChange={(e)=>handlename(e)} ></input>
-				</Form.Item></div></Col>
-			<Col span={12} ><div>
-				<Form.Item label="State" name="state">
+		const list1 = {
+			name: input.Name,
+			desc: input.Description,
+			priority: input.Priority,
+			author: input.Author,
+			created: input.Created,
+			lastModified: input.LastModified,
+			state: input.State,
+		};
 
-					<Switch className="sw-button" autofocus="true" checkedChildren="is enabled" unCheckedChildren="not enabled" defaultChecked />
-
-				</Form.Item></div></Col>
-
-		</Row>
-		<Row gutter={[64, 40]}>
-			<Col span={24} >
-				<div>
-					<Form.Item label="Description" >
-						<TextArea class="desc-inp" name="desc_inp" showCount maxLength={1000} onChange={(e)=>handledesc(e) }>
-						</TextArea>
-					</Form.Item>
-				</div>
-			</Col>
-		</Row>
+		update(list1).then((list1) => {
+			console.log(list1)
+			this.setState({ values: list1 });
+		});
+	}
+	console.log(values);
+	const [name, setname] = useState('');
+	const [desc, setdesc] = useState('');
+	const [prio, setprio] = useState('');
+	const [author, setauthor] = useState('');
+	const [create, setcreate] = useState('');
+	const [lastm, setlastm] = useState('');
 
 
-		<Row gutter={[64, 40]}>
-			<Col span={12} >
-				<div>
-					<FormItem name="priority" label="Priority" required tooltip="This is a required field">
-						<input class="prio-inp" placeholder="1000" onChange={(e)=>handleprio(e)}/>
-					</FormItem>
-				</div>
-			</Col>
-			<Col span={12} >
-				<div>
-					<FormItem label="Author" name="author">
-						<p class="author" >System</p>
+	const handlename = (event) => {
+		// this.setState({name: event.target.value })
+		const Name = event.target.value;
+		console.log(Name);
+		setname(Name);
+	}
+	const handledesc = (event) => {
+		const description = event.target.value;
+		console.log(description);
+		setname(description);
+	}
+	const handleprio = (event) => {
+		const prior = event.target.value;
+		console.log(prior);
+		setname(prior);
+	}
+	const submituser = async (e) => {
+		//not complete....
+		//postrequest()
 
-					</FormItem>
-				</div>
-			</Col>
-		</Row>
-		<Row gutter={[64, 40]}>
-			<Col span={12} >
-				<div>
-					<FormItem label="Created" name="create">
-						<p class="create">Saturday, October 15, 2019,18:45:23</p>
-					</FormItem>
-				</div>
-			</Col>
+	}
 
-			<Col span={12} >
-				<div>
-					<FormItem label="Last modified" name="lastm">
-						<p className="lastm">Saturday, October 15, 2019,18:45:23</p>
-					</FormItem>
-				</div>
-			</Col>
-		</Row>
-		<Row gutter={[64, 40]}>
-			<Col span={24} >
-				<div>
-					<Form.Item >
-						<button class ="submitbutton" type="submit" >submit</button>
-					</Form.Item>
-				</div>
-			</Col>
-		</Row>
-	</Form>
-	</div>;
-}
+	return (
+		values.map((inp) => (
+		
+		<Form onSubmit={submituser} name='basic form' layout="vertical" size="middle" onFinish={(values) => this.handleFormSubmit(values)}>
+			<h1 class="title">General Setting</h1>
+			<Row gutter={[64, 40]}>
+				<Col span={12} ><div>
+					<Form.Item label="Name" name="Name" rules={[{ required: true, message: 'Please enter your name' }]}>
+						<input name="name" className="name" onChange={(e) => handlename(e)} placeholder={inp.name}></input>
+					</Form.Item></div></Col>
+				<Col span={12} ><div>
+					<Form.Item label="State" name="state">
+
+						<Switch className="sw-button" autofocus="true" checkedChildren="is enabled" unCheckedChildren="not enabled" defaultChecked />
+
+					</Form.Item></div></Col>
+
+			</Row>
+			<Row gutter={[64, 40]}>
+				<Col span={24} >
+					<div>
+						<Form.Item label="Description" >
+							<TextArea class="desc-inp" name="desc_inp" showCount maxLength={1000} onChange={(e) => handledesc(e)} value={inp.desc}>
+							</TextArea>
+						</Form.Item>
+					</div>
+				</Col>
+			</Row>
+
+
+			<Row gutter={[64, 40]}>
+				<Col span={12} >
+					<div>
+						<FormItem name="priority" label="Priority" required tooltip="This is a required field">
+							<input class="prio-inp" onChange={(e) => handleprio(e)} placeholder={inp.priority}/>
+						</FormItem>
+					</div>
+				</Col>
+				<Col span={12} >
+					<div>
+						<FormItem label="Author" name="author">
+							<p class="author" >{inp.author}</p>
+
+						</FormItem>
+					</div>
+				</Col>
+			</Row>
+			<Row gutter={[64, 40]}>
+				<Col span={12} >
+					<div>
+						<FormItem label="Created" name="create">
+							<p class="create">Saturday, October 15, 2019,18:45:23</p>
+						</FormItem>
+					</div>
+				</Col>
+
+				<Col span={12} >
+					<div>
+						<FormItem label="Last modified" name="lastm">
+							<p className="lastm">Saturday, October 15, 2019,18:45:23</p>
+						</FormItem>
+					</div>
+				</Col>
+			</Row>
+			<Row gutter={[64, 40]}>
+				<Col span={24} >
+					<div>
+						<Form.Item >
+							<button class="submitbutton" type="submit" >submit</button>
+						</Form.Item>
+					</div>
+				</Col>
+			</Row>
+		</Form>
+
+	))
+	)
+};
 export default MyForm;
 
